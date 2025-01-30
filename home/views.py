cf19e6fabaeda251657a6c2b76c1518a5f55d586
waniarslan786 <waniarslan250@gmail.com>
2025-01-30 12:00:15 +0530

    initial

---
+++ home/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def team(request):
+    return render(request, 'team.html')
+
